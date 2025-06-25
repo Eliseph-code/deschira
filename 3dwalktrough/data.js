d021544be59e52385c9ca57e3aec8676dcd97c1b
Eliseph-code <elisephilips8@gmail.com>
2025-06-25 12:12:33 +0200

    Add files via upload

---
+++ 3dwalktrough/data.js
@@ -0,0 +1,611 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entreestore",
+      "name": "entreestore",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.5271675352884593,
+        "pitch": 0.032864289667944746,
+        "fov": 1.4438271227947086
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7518563999648906,
+          "pitch": 0.3353984049318779,
+          "rotation": 0,
+          "target": "1-centerstore"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0362308217951472,
+          "pitch": -0.689844764387443,
+          "title": "Acosorb",
+          "text": "GreenLine"
+        },
+        {
+          "yaw": 1.3152566313519891,
+          "pitch": -0.5589590924219383,
+          "title": "LillyLite",
+          "text": "Akari grapes chandelier"
+        },
+        {
+          "yaw": -1.9709558575984758,
+          "pitch": 0.1857385504811404,
+          "title": "Wine &amp; Barrels",
+          "text": "Winerex - Lola"
+        },
+        {
+          "yaw": 0.9476041361350802,
+          "pitch": 0.2776320211703833,
+          "title": "Wine &amp; Barrels",
+          "text": "Caverack - Abra"
+        },
+        {
+          "yaw": 1.4638599851974092,
+          "pitch": 0.8074583529980952,
+          "title": "Maatwerk",
+          "text": "Customization"
+        },
+        {
+          "yaw": -0.5072310855294369,
+          "pitch": 0.9992006795489345,
+          "title": "Villeroy &amp; Boch&nbsp;",
+          "text": "Bourgogna Garden Anthracite"
+        },
+        {
+          "yaw": -0.2994010109918328,
+          "pitch": 0.3717832226112705,
+          "title": "Wine &amp; Barrels",
+          "text": "Mensolas - Display"
+        },
+        {
+          "yaw": -2.763658086266126,
+          "pitch": 0.5865954980936223,
+          "title": "Wine &amp; Barrels",
+          "text": "Winerex - Faro"
+        },
+        {
+          "yaw": -2.455588738217095,
+          "pitch": -0.4224010110976053,
+          "title": "Omexo",
+          "text": "Khatam"
+        },
+        {
+          "yaw": -1.292367003154908,
+          "pitch": 0.07829558778306378,
+          "title": "Little Greene",
+          "text": "Deep Space Blue"
+        },
+        {
+          "yaw": 2.64146775192836,
+          "pitch": -0.9764755217361181,
+          "title": "Maatwerk Derby Steenstrips",
+          "text": "Customized Derby Stone Strips"
+        },
+        {
+          "yaw": -2.477757146022025,
+          "pitch": -1.1271255769338993,
+          "title": "Delta Light",
+          "text": "Spyco"
+        }
+      ]
+    },
+    {
+      "id": "1-centerstore",
+      "name": "centerstore",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.721751026621746,
+          "pitch": 0.2941099152526583,
+          "rotation": 0,
+          "target": "0-entreestore"
+        },
+        {
+          "yaw": 0.6237166614549636,
+          "pitch": 0.15064501769601435,
+          "rotation": 0,
+          "target": "2-dining"
+        },
+        {
+          "yaw": 1.4561425151026777,
+          "pitch": 0.07954643075922796,
+          "rotation": 0,
+          "target": "4-sanitair"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3093017838636847,
+          "pitch": -0.690220532029107,
+          "title": "Acosorb",
+          "text": "GreenLine"
+        },
+        {
+          "yaw": -3.012443262709464,
+          "pitch": -0.4798307538026023,
+          "title": "Little Greene",
+          "text": "Deep Space Blue"
+        },
+        {
+          "yaw": 2.753923951216386,
+          "pitch": -0.5451776215353945,
+          "title": "Omexo",
+          "text": "Khatam"
+        },
+        {
+          "yaw": 2.6041922460448435,
+          "pitch": 0.4521339023848956,
+          "title": "Wine &amp; Barrels",
+          "text": "Winerex - Dia"
+        },
+        {
+          "yaw": -2.4028847566553218,
+          "pitch": 0.8609265783139506,
+          "title": "Maatwerk",
+          "text": "Customization"
+        },
+        {
+          "yaw": -2.9519236805822366,
+          "pitch": 0.20180833175505697,
+          "title": "Maatwerk",
+          "text": "Customization"
+        },
+        {
+          "yaw": -1.4063726764272602,
+          "pitch": -1.1506118022839793,
+          "title": "Artemis",
+          "text": "Artistic Wine Glas Pendant"
+        },
+        {
+          "yaw": -0.5446733945527065,
+          "pitch": 0.7118390419641969,
+          "title": "Leta",
+          "text": "Customized Bar Stool - Arvenol"
+        },
+        {
+          "yaw": 2.277351629176433,
+          "pitch": 0.6623288364255089,
+          "title": "Wine &amp; Barrels",
+          "text": "Faro"
+        },
+        {
+          "yaw": 2.7865376088048066,
+          "pitch": -1.2728902011504957,
+          "title": "Delta light",
+          "text": "Spyco"
+        },
+        {
+          "yaw": -1.924430820657257,
+          "pitch": -0.47066138925162093,
+          "title": "Maatwerk Derby Steenstrips",
+          "text": "Customized Derby Stone Strips"
+        },
+        {
+          "yaw": -1.454642060479589,
+          "pitch": 1.0797741025556427,
+          "title": "Villeroy &amp; Boch&nbsp;",
+          "text": "Bourgogna Garden Anthracite"
+        }
+      ]
+    },
+    {
+      "id": "2-dining",
+      "name": "dining",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9958087843256571,
+          "pitch": 0.5340760392239154,
+          "rotation": 0,
+          "target": "3-bar"
+        },
+        {
+          "yaw": 0.5048110817753351,
+          "pitch": 0.3465621572753488,
+          "rotation": 0,
+          "target": "1-centerstore"
+        },
+        {
+          "yaw": -0.6634499155669786,
+          "pitch": 0.18867027628516198,
+          "rotation": 0,
+          "target": "4-sanitair"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.4797239901616557,
+          "pitch": 0.10776718468442859,
+          "title": "Leta",
+          "text": "Customized Furniture"
+        },
+        {
+          "yaw": -1.1052088059817944,
+          "pitch": 0.20388676507542414,
+          "title": "Leta",
+          "text": "M3400"
+        },
+        {
+          "yaw": -1.4303447456568712,
+          "pitch": 0.5194417864148502,
+          "title": "Leta",
+          "text": "Customized - B7000&nbsp;"
+        },
+        {
+          "yaw": -1.4016386295358174,
+          "pitch": -0.09511871860889087,
+          "title": "Omexo",
+          "text": "Khatam"
+        },
+        {
+          "yaw": -1.4242271724095552,
+          "pitch": -0.7713041697055587,
+          "title": "Little Greene",
+          "text": "Mochi"
+        },
+        {
+          "yaw": -0.7049232766358084,
+          "pitch": -0.6992197282544659,
+          "title": "Wever &amp; Ducré",
+          "text": "Mirro Suspended"
+        },
+        {
+          "yaw": -0.14799402298956998,
+          "pitch": -0.8245159602408592,
+          "title": "Acosorb",
+          "text": "Greenline"
+        }
+      ]
+    },
+    {
+      "id": "3-bar",
+      "name": "bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.241076533171748,
+          "pitch": 0.5320150628806424,
+          "rotation": 0,
+          "target": "2-dining"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.971545192546854,
+          "pitch": 0.5818414903715716,
+          "title": "Fentistone",
+          "text": "Composiet"
+        },
+        {
+          "yaw": -1.9704089222092733,
+          "pitch": 0.27573916715527247,
+          "title": "Pfleiderer",
+          "text": "Orchidea"
+        },
+        {
+          "yaw": -1.919384235369261,
+          "pitch": 1.0965470292544524,
+          "title": "Pfleiderer",
+          "text": "Terrazzo Bronze"
+        },
+        {
+          "yaw": 1.1084118153006202,
+          "pitch": 0.26775520056430224,
+          "title": "Roestvrijstaal",
+          "text": "Stainless Steel"
+        },
+        {
+          "yaw": 0.7139844131171067,
+          "pitch": 0.2173632459173298,
+          "title": "Elkay",
+          "text": "Sink &amp; Faucet"
+        },
+        {
+          "yaw": 0.05027744797340006,
+          "pitch": 0.09449981139708541,
+          "title": "Meiko",
+          "text": "Glass Washer"
+        },
+        {
+          "yaw": 1.241094819462086,
+          "pitch": 0.5793293665362622,
+          "title": "Liebher",
+          "text": "Vinothek"
+        },
+        {
+          "yaw": -0.006469258612796125,
+          "pitch": 0.49350242668992905,
+          "title": "Liebher",
+          "text": "Freezer"
+        },
+        {
+          "yaw": 1.7480576500687413,
+          "pitch": 0.5223540541608891,
+          "title": "Wine &amp; Barrels / Pevino",
+          "text": "Pevino Imperial Giant"
+        },
+        {
+          "yaw": -1.8821149386812657,
+          "pitch": -0.7613592527246063,
+          "title": "Artemis",
+          "text": "Artistic Wine Glass Pendant"
+        }
+      ]
+    },
+    {
+      "id": "4-sanitair",
+      "name": "sanitair",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9531694996023745,
+          "pitch": 0.791733844600909,
+          "rotation": 0,
+          "target": "5-toilet"
+        },
+        {
+          "yaw": 1.8845912460645042,
+          "pitch": 0.658295945292382,
+          "rotation": 0,
+          "target": "1-centerstore"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0048980546348876,
+          "pitch": -0.24255708888457583,
+          "title": "Sustonable",
+          "text": "Cocoa Empire Nero"
+        },
+        {
+          "yaw": -0.7685549571928636,
+          "pitch": -1.4515649173489447,
+          "title": "Homapal",
+          "text": "Aluminium - Hpl"
+        },
+        {
+          "yaw": -0.30958995266559164,
+          "pitch": -0.061473102267854074,
+          "title": "Laufen",
+          "text": "Frame 25"
+        },
+        {
+          "yaw": -0.1428872736366884,
+          "pitch": 0.2792666718119996,
+          "title": "Fontana Showers",
+          "text": "Touchless Sensor Faucet With Soap Dispencer"
+        },
+        {
+          "yaw": 0.5481672584232182,
+          "pitch": -0.10685726612311797,
+          "title": "Kwc",
+          "text": "Exos - Towel Dispencer"
+        },
+        {
+          "yaw": 0.6383711231401339,
+          "pitch": 0.817272043961383,
+          "title": "Kwc",
+          "text": "Stainless Steel Bin"
+        },
+        {
+          "yaw": -0.2739236269776715,
+          "pitch": 1.541692483330161,
+          "title": "Villeroy &amp; Boch",
+          "text": "Bourgogna Garden Greige"
+        },
+        {
+          "yaw": 2.787383453018905,
+          "pitch": 0.10794038440619502,
+          "title": "Little Greene",
+          "text": "Deep Space Blue"
+        },
+        {
+          "yaw": -0.23900398267768175,
+          "pitch": -1.0848243076628705,
+          "title": "Delta Light",
+          "text": "Entero"
+        }
+      ]
+    },
+    {
+      "id": "5-toilet",
+      "name": "toilet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.574790051715354,
+          "pitch": 0.7781203530662495,
+          "rotation": 0,
+          "target": "4-sanitair"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6202910721802315,
+          "pitch": -0.4313794122762431,
+          "title": "Little Greene",
+          "text": "Mocht"
+        },
+        {
+          "yaw": -0.03752976712823042,
+          "pitch": -0.3199749187291516,
+          "title": "Sustonable",
+          "text": "Cocoa Empire Nero"
+        },
+        {
+          "yaw": -0.7539352460206707,
+          "pitch": -0.4239324408902405,
+          "title": "Little Greene",
+          "text": "Deep Space Blue"
+        },
+        {
+          "yaw": -0.03735744475565994,
+          "pitch": 0.101781378817277,
+          "title": "Tece",
+          "text": "Touchless Control Pannel"
+        },
+        {
+          "yaw": 0.02358628691512976,
+          "pitch": 0.8022834842103901,
+          "title": "Villeroy &amp; Boch",
+          "text": "Subway 2.0 Deep Flush Closed"
+        },
+        {
+          "yaw": -0.5316784386732536,
+          "pitch": 0.7364765540058773,
+          "title": "Kwc",
+          "text": "Rodan - Stainless Steel Bin"
+        },
+        {
+          "yaw": -0.17017165573337145,
+          "pitch": 0.5232933811862193,
+          "title": "Kwc",
+          "text": "Exos - Stainless Steel Toilet Brush"
+        },
+        {
+          "yaw": 0.6208347871554984,
+          "pitch": 0.402688159054744,
+          "title": "Kwc",
+          "text": "Stratos - Stainless Steel Toilet Paper Holder"
+        },
+        {
+          "yaw": 2.7756002052694324,
+          "pitch": -1.1529738336459232,
+          "title": "Homapal",
+          "text": "Aluminium - Hpl"
+        },
+        {
+          "yaw": 2.047320571940192,
+          "pitch": -0.7849602352516634,
+          "title": "Delta Light",
+          "text": "Entero"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
